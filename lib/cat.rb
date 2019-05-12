cc6f7d1c2247f935b45ea4233fa6f5f8155fbe4e
Ibrahim  Akar <ibsakar@gmail.com>
2019-05-12 20:27:19 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,33 @@
 class Cat
-  # code goes here
+
+attr_accessor :owner 
+attr_reader   :owner
+
+def initialize(cat,name)
+  @name = name
+  @cat = cat
+  end
+
+  def initialize(name)
+    end
+
+
+def owner=(owner)
+  @owner = owner
+  #owner.add_pet(self)
 end
+
+def owner
+  @owner
+end
+
+end
+#def initialize (name, genre)
+#  @name = name
+#  @genre = genre
+
+#def artist_name
+#  self.artist.name
+#end
+
+#end
